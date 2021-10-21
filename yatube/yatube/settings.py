cd107e616ecee44544202e4d7590c45ddd2e39c0
Maxim <sokmaxal@gmail.com>
2021-10-21 12:52:05 +0300

    Posts application created

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'posts.apps.PostsConfig', #создание приложения posts
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
