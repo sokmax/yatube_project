e3b2ea79ff484c2fdd7f06e5b9cfec531d946c5c
Maxim <sokmaxal@gmail.com>
2021-10-23 17:46:41 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Главная страница
+def index(request):
+    return HttpResponse('VERY Главная страница')
+
+# Страница с груповыми постами
+def group_posts(request, slug):
+    return HttpResponse('These are group_posts')
+
